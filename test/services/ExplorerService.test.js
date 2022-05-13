a3a51a8cfa5369d6279bc174d8d2c2db6c857d28
Alejandro Maya <alejandromayamtz@gmail.com>
2022-05-12 22:20:19 -0500

    Corrigiendo syntaxis con eslint en todos nuestros archivos

--- test/services/ExplorerService.test.js
+++ test/services/ExplorerService.test.js
@@ -2,29 +2,29 @@ const ExplorerService = require("./../../lib/services/ExplorerService");
 const Reader = require("./../../lib/utils/Reader");
 
 
-describe('Test for ExplorerService', () => {
-    test('Test for method filterByMission', () =>{
+describe("Test for ExplorerService", () => {
+    test("Test for method filterByMission", () =>{
         const explorers1 = Reader.readJsonFile("explorers.json");
         const explorer = ExplorerService.filterByMission(explorers1, "node");
         expect(explorer).not.toBeUndefined();
     });
-    test('Test for method getAmountOfExplorersByMission', () =>{
+    test("Test for method getAmountOfExplorersByMission", () =>{
         const explorers1 = Reader.readJsonFile("explorers.json");
-        const explorer = ExplorerService.getAmountOfExplorersByMission(explorers1, "node")
+        const explorer = ExplorerService.getAmountOfExplorersByMission(explorers1, "node");
         expect(explorer).toBe(10);
     });
-    test('Test for method getAmountOfExplorersByMission', () =>{
+    test("Test for method getAmountOfExplorersByMission", () =>{
         const explorers1 = Reader.readJsonFile("explorers.json");
-        const explorer = ExplorerService.getExplorersUsernamesByMission(explorers1, "node")
-        expect(explorer).toStrictEqual(['ajolonauta1',
-                               'ajolonauta2',
-                               'ajolonauta3',
-                               'ajolonauta4',
-                               'ajolonauta5',
-                               'ajolonauta11',
-                               'ajolonauta12',
-                               'ajolonauta13', 
-                               'ajolonauta14',
-                               'ajolonauta15']);
+        const explorer = ExplorerService.getExplorersUsernamesByMission(explorers1, "node");
+        expect(explorer).toStrictEqual(["ajolonauta1",
+            "ajolonauta2",
+            "ajolonauta3",
+            "ajolonauta4",
+            "ajolonauta5",
+            "ajolonauta11",
+            "ajolonauta12",
+            "ajolonauta13", 
+            "ajolonauta14",
+            "ajolonauta15"]);
     });
 });
--- test/utils/Reader.test.js
+++ test/utils/Reader.test.js
@@ -1,7 +1,7 @@
 const Reader = require("./../../lib/utils/Reader");
 
-describe('Test for Reader', () => {
-    test('1. Read a JSON', () => {
+describe("Test for Reader", () => {
+    test("1. Read a JSON", () => {
         const explorer = Reader.readJsonFile("explorers.json");
         expect(explorer).not.toBeUndefined();
     });
